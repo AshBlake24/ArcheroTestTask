28e25020d1057195ca42466ec46a80afb2d1c5b0
Roman Ridel <ridel_rv@mail.ru>
2023-07-25 17:40:10 +0300

    Add helper classes

---
+++ Assets/Scripts/Utilities/TimeUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.Utilities
+{
+    public static class TimeUtility
+    {
+        private static readonly Dictionary<float, WaitForSeconds> s_waitDictionary = new Dictionary<float, WaitForSeconds>();
+        
+        public static WaitForSeconds GetTime(float timeInSeconds)
+        {
+            if (s_waitDictionary.TryGetValue(timeInSeconds, out WaitForSeconds wait))
+                return wait;
+
+            s_waitDictionary[timeInSeconds] = new WaitForSeconds(timeInSeconds);
+
+            return s_waitDictionary[timeInSeconds];
+        }
+    }
+}

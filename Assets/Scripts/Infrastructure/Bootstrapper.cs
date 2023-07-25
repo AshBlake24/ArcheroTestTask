d40e0fac736b07b37b2b682f67ebd1581a2c60ac
Roman Ridel <ridel_rv@mail.ru>
2023-07-25 18:41:05 +0300

    Add loading screen

--- Assets/Scripts/Infrastructure/Bootstrapper.cs
+++ Assets/Scripts/Infrastructure/Bootstrapper.cs
@@ -5,11 +5,15 @@ namespace Source.Infrastructure
 {
     public class Bootstrapper : MonoBehaviour, ICoroutineRunner
     {
+        [SerializeField] private LoadingScreen _loadingScreen;
+        
         private Game _game;
 
         private void Awake()
         {
-            _game = new Game(this);
+            LoadingScreen loadingScreen = Instantiate(_loadingScreen);
+            
+            _game = new Game(this, loadingScreen);
             _game.StateMachine.Enter<BootstrapState>();
             
             DontDestroyOnLoad(this);
---
+++ Assets/Scripts/Infrastructure/LoadingScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using Source.Utilities;
+using UnityEngine;
+
+namespace Source.Infrastructure
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _fadeTime;
+        [SerializeField] private float _updateTime;
+
+        private void Awake() => 
+            DontDestroyOnLoad(this);
+
+        public void Hide() => 
+            StartCoroutine(FadeOut());
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            _canvasGroup.alpha = 1;
+        }
+
+        private IEnumerator FadeOut()
+        {
+            while (_canvasGroup.alpha > _fadeTime)
+            {
+                _canvasGroup.alpha -= _fadeTime;
+                yield return TimeUtility.GetTime(_updateTime);
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}

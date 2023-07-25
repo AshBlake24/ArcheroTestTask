5eb8d0ab33d2175510ab3b5801aa6cbf9e9400e4
Roman Ridel <ridel_rv@mail.ru>
2023-07-25 16:28:50 +0300

    Add player movement

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Source.Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _positionOffset;
+        
+        private Transform _target;
+
+        public void SetTarget(Transform target)
+        {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+            
+            _target = target;
+        }
+
+        private void LateUpdate()
+        {
+            if (_target == null)
+                return;
+
+            transform.position = GetPosition();
+        }
+
+        private Vector3 GetPosition()
+        {
+            Vector3 position = _target.position;
+            position.x = 0;
+            position += _positionOffset;
+            return position;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Source.Camera;
+using Source.Infrastructure;
+using Source.Infrastructure.Services.Input;
+using UnityEngine;
+
+namespace Source.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private CharacterController _characterController;
+        
+        private IInputService _inputService;
+        private Vector3 _direction;
+
+        private void Awake()
+        {
+            _inputService = Game.InputService;
+        }
+
+        private void Start()
+        {
+            UnityEngine.Camera.main.GetComponent<CameraFollow>().SetTarget(transform);
+        }
+
+        private void Update()
+        {
+            if ((_inputService.Axis.sqrMagnitude > 0.01f) == false) 
+                return;
+            
+            _direction = GetDirection();
+            _direction.Normalize();
+                
+            RotateTowardsDirection();
+            Move();
+        }
+
+        private Vector3 RotateTowardsDirection()
+        {
+            return transform.forward = _direction;
+        }
+
+        private void Move() => 
+            _characterController.Move(_direction * _moveSpeed * Time.deltaTime);
+
+        private Vector3 GetDirection()
+        {
+            Vector2 inputServiceAxis = _inputService.Axis;
+            return new Vector3(inputServiceAxis.x, 0, inputServiceAxis.y);
+        }
+    }
+}

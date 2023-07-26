59791cc86eb56f3b4d67dd311bfd46d202ab7ac3
Roman Ridel <ridel_rv@mail.ru>
2023-07-26 13:37:00 +0300

    Add enemy death

---
+++ Assets/Scripts/Enemies/EnemyDeath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using Source.Utilities;
+using UnityEngine;
+
+namespace Source.Enemies
+{
+    [RequireComponent(typeof(EnemyHealth))]
+    public class EnemyDeath : MonoBehaviour
+    {
+        [SerializeField] private EnemyHealth _health;
+        [SerializeField] private float _timeToDestroy = 3f;
+
+        public event Action Died;
+
+        private void Start() => 
+            _health.HealthChanged += OnHealthChanged;
+
+        private void OnDestroy() => 
+            _health.HealthChanged -= OnHealthChanged;
+
+        private void OnHealthChanged()
+        {
+            if (_health.CurrentValue <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            StartCoroutine(DestroyTimer());
+            
+            Died?.Invoke();
+        }
+
+        private IEnumerator DestroyTimer()
+        {
+            yield return TimeUtility.GetTime(_timeToDestroy);
+            
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -1,32 +1,29 @@
 ﻿using System;
+using Source.Combat;
 using UnityEngine;
 
 namespace Source.Enemies
 {
-    public class EnemyHealth : MonoBehaviour
+    public class EnemyHealth : MonoBehaviour, IDamageable
     {
         public int _maxValue;
         public int _currentValue;
 
         public event Action HealthChanged;
 
-        public int CurrentValue
-        {
-            get => _currentValue;
-            private set
-            {
-                if (_currentValue != value)
-                {
-                    _currentValue = value;
-                    HealthChanged?.Invoke();
-                }
-            }
-        }
+        public int CurrentValue => _currentValue;
+        
+        private void Start() => 
+            _currentValue = _maxValue;
 
-        public int MaxValue
+        public void TakeDamage(int damage)
         {
-            get => _maxValue;
-            private set => _maxValue = value;
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage), "Damage must not be less than 0");
+
+            _currentValue = Mathf.Max(CurrentValue - damage, 0);
+            
+            HealthChanged?.Invoke();
         }
     }
 }

78bf1133329138f1e61cfaf57a399d6d2e650c01
Roman Ridel <ridel_rv@mail.ru>
2023-07-27 11:24:11 +0300

    Add countdown timer

---
+++ Assets/Scripts/UI/TimeCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using Source.Utilities;
+using TMPro;
+using UnityEngine;
+
+namespace Source.UI
+{
+    public class TimeCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        
+        public int _secondsToStart;
+
+        public void Construct(int secondsToStart)
+        {
+            _secondsToStart = secondsToStart;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(Countdown());
+        }
+
+        private IEnumerator Countdown()
+        {
+            while (_secondsToStart > 0)
+            {
+                _text.text = _secondsToStart.ToString();
+                
+                yield return TimeUtility.GetTime(1);
+                
+                _secondsToStart--;
+            }
+            
+            
+        }
+    }
+}

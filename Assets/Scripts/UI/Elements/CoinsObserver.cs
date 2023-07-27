d1e57af949832948c7473ddf459caad11024ad82
Roman Ridel <ridel_rv@mail.ru>
2023-07-27 19:14:06 +0300

    Add coins observer

---
+++ Assets/Scripts/UI/Elements/CoinsObserver.cs
@@ -0,0 +1,27 @@
+﻿using Source.Data;
+using TMPro;
+using UnityEngine;
+
+namespace Source.UI.Elements
+{
+    public class CoinsObserver : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _coins;
+        
+        private Balance _balance;
+
+        public void Construct(Balance balance)
+        {
+            _balance = balance;
+            _balance.Changed += OnBalanceChanged;
+        }
+
+        private void OnDestroy() => 
+            _balance.Changed -= OnBalanceChanged;
+
+        private void Start() => OnBalanceChanged();
+
+        private void OnBalanceChanged() => 
+            _coins.text = _balance.Coins.ToString();
+    }
+}

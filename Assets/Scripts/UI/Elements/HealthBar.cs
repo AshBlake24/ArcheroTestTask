223bc4450db5b881e6e5164c6a7bad04f0ecd19c
Roman Ridel <ridel_rv@mail.ru>
2023-07-27 19:47:39 +0300

    Remove health text

--- Assets/Scripts/UI/Elements/HealthBar.cs
+++ Assets/Scripts/UI/Elements/HealthBar.cs
@@ -7,12 +7,8 @@ namespace Source.UI.Elements
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] private Image _currentHealthImage;
-        [SerializeField] private TextMeshProUGUI _currentHealthText;
 
-        public void SetValue(int current, int max)
-        {
+        public void SetValue(int current, int max) => 
             _currentHealthImage.fillAmount = (float) current / max;
-            //_currentHealthText.text = $"{current}/{max}";
-        }
     }
 }
